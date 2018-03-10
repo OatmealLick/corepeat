9b82ecb6460b085a9194342762c11481587e4bbd
Lukasz Sciga <lsciga@akamai.com>
2018-03-10 17:27:34 +0100

    added mock url to conf service

--- ng-corepeat/src/app/configuration.service.ts
+++ ng-corepeat/src/app/configuration.service.ts
@@ -6,6 +6,14 @@ export class ConfigurationService {
   constructor() { }
 
   getUrl(): string {
+    return this.getMockUrl();
+  }
+
+  getAppUrl(): string {
     return "http://localhost:8080";
+  }
+
+  getMockUrl(): string {
+    return "api";
   }
 }
